4437d4f6f796da79c58972c9bcf1337a4984ef1f
jhorman10 <jsojhor@gmail.com>
2020-01-24 20:08:49 -0500

    upload proyect

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ButtonBackToHome } from '../components/ButtonBackToHome'
+
+export const NotFound = () => (
+    <div>
+        <h1 className = 'title'>
+            <b>404</b>
+        </h1>
+        <p className = 'subtitle'>¡Not Found!</p>
+        <ButtonBackToHome/>
+    </div>   
+)
